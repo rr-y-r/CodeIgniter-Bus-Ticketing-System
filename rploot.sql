00ca0b32b30a6b60c20086780fa938494d68f358
rr-y-r <rr.y.r@gmail.com>
2015-04-30 05:54:25 +0700

    init new SQL file

--- rploot.sql
+++ rploot.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: 29 Apr 2015 pada 04.21
+-- Generation Time: 29 Apr 2015 pada 15.53
 -- Versi Server: 5.5.40
 -- PHP Version: 5.4.34
 
@@ -50,8 +50,7 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 --
 
 INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('cdc007f12f34b1c94ad6975879a9719a', '::1', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36', 1430300925, 'a:5:{s:9:"user_data";s:0:"";s:5:"email";s:5:"admin";s:6:"userid";N;s:12:"is_logged_in";b:1;s:8:"is_admin";b:0;}'),
-('fa8c4347f5b20c442a4553e876c1727a', '::1', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36', 1430306088, 'a:5:{s:9:"user_data";s:0:"";s:5:"email";s:9:"mahasiswa";s:6:"userid";s:1:"2";s:12:"is_logged_in";b:1;s:5:"level";s:9:"mahasiswa";}');
+('712e572c2e651bafbb7fb14647af81be', '::1', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36', 1430347571, 'a:4:{s:5:"email";s:9:"mahasiswa";s:6:"userid";s:1:"2";s:12:"is_logged_in";b:1;s:5:"level";s:9:"mahasiswa";}');
 
 -- --------------------------------------------------------
 
@@ -75,6 +74,43 @@ INSERT INTO `dorm_room` (`roomid`, `nomor`, `fasilitas`, `status`, `kapasitas`) 
 (31, 88, 'dijwi', 'awdawd', 1),
 (32, 111, '1', 'asdasd', 1),
 (33, 44, '44', 'vbhjck', 44);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `files`
+--
+
+CREATE TABLE IF NOT EXISTS `files` (
+`id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `userid` varchar(100) DEFAULT NULL,
+  `url` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `files`
+--
+
+INSERT INTO `files` (`id`, `filename`, `title`, `userid`, `url`) VALUES
+(9, 'download_(1).jpg', '33', '2', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ticket`
+--
+
+CREATE TABLE IF NOT EXISTS `ticket` (
+`ticketid` int(5) NOT NULL,
+  `jenis` varchar(13) NOT NULL,
+  `userid` int(5) NOT NULL,
+  `deskripsi` varchar(32) NOT NULL,
+  `status` varchar(13) NOT NULL,
+  `lampiran` varchar(100) NOT NULL,
+  `file` varchar(180) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -125,6 +161,18 @@ ALTER TABLE `dorm_room`
  ADD PRIMARY KEY (`roomid`);
 
 --
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+ ADD PRIMARY KEY (`ticketid`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -144,6 +192,16 @@ MODIFY `id_bank` int(8) NOT NULL AUTO_INCREMENT;
 --
 ALTER TABLE `dorm_room`
 MODIFY `roomid` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+MODIFY `ticketid` int(5) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
